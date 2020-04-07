1d547be5e0f5fbae34e010eed0f5359b453af174
unknown <sukhpreet-s>
2020-04-07 00:56:18 -0400

    Updates

---
+++ precache-manifest.a205c26af657ba16788688bf7fbc197c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f3ce32c91aa61ef79920869820a39f",
+    "url": "/react-demo-post-app/index.html"
+  },
+  {
+    "revision": "2a909b624f58aa3ddf7a",
+    "url": "/react-demo-post-app/static/css/main.a409d2bf.chunk.css"
+  },
+  {
+    "revision": "b0bfc5343a01c2dc2f5b",
+    "url": "/react-demo-post-app/static/js/2.c643d06c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo-post-app/static/js/2.c643d06c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a909b624f58aa3ddf7a",
+    "url": "/react-demo-post-app/static/js/main.d42f1ee1.chunk.js"
+  },
+  {
+    "revision": "de31399d3d18a168fabb",
+    "url": "/react-demo-post-app/static/js/runtime-main.e9f89078.js"
+  }
+]);
